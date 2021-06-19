b2ccf005ca3cdc841d38c166589da05fdaa283f2
sigma-andex <sigma.andex@pm.me>
2021-06-19 12:29:29 +0100

    WIP

--- src/Data/FastVect/Add.purs
+++ src/Data/FastVect/Add.purs
@@ -2,9 +2,14 @@ module Data.FastVect.Add where
 
 
 import Prim.Symbol (class Cons)
+import Type.Prelude (Proxy(..))
 
 
-class AddSingle (augend :: Symbol) (addend :: Symbol) (carry :: Symbol) (sum :: Symbol) | augend addend -> carry sum 
+class AddSingle (augend :: Symbol) (addend :: Symbol) (carry :: Symbol) (sum :: Symbol) 
+    | augend addend -> carry sum 
+    , augend carry -> sum addend
+    , addend carry -> sum augend
+-- AddSingle "4" t88 "0" t100 => Proxy "4"
 
 
 instance AddSingle "0" "0" "0" "0"
@@ -164,21 +169,104 @@ else instance AddIntermediate "1" "8" "1" "1" "9"
 -- , AddSingle augendHead tailCarry x carry 
 -- ) => AddHelper1 augendHead augendTail "0" addendTail carry sum 
 
-class AddHelper (augendHead :: Symbol) (augendTail :: Symbol) (addendHead :: Symbol) (addendTail :: Symbol) (carry :: Symbol) (sum :: Symbol) | augendHead augendTail addendHead addendTail -> carry sum 
+class AddHelper (augendHead :: Symbol) (augendTail :: Symbol) (addendHead :: Symbol) (addendTail :: Symbol) (carryHead :: Symbol) (carryTail :: Symbol) (sumHead :: Symbol) (sumTail :: Symbol) 
+    | augendHead augendTail addendHead addendTail carryTail -> carryHead sumHead sumTail
+    , augendHead augendTail carryHead sumHead sumTail -> addendHead addendTail carryTail
+    , addendHead addendTail carryHead sumHead sumTail -> augendHead augendTail carryTail
 
-instance (AddSingle augendHead addendHead carry sum) => AddHelper augendHead "" addendHead "" carry sum
+instance (AddSingle augendHead addendHead carry sum) => AddHelper augendHead "" addendHead "" carry "" sumHead ""
+{-
+
+    t98 could not be determined
+    t120 could not be determined
+    t121 could not be determined
+    t128 could not be determined
+    t135 could not be determined
+    t136 could not be determined
+    t139 could not be determined
+
+    forall (t98 :: Symbol) (t120 :: Symbol) (t121 :: Symbol) (t128 :: Symbol) (t135 :: Symbol) (t136 :: Symbol) (t139 :: Symbol). Cons t120 t121 t98 
+    => AddSingle "1" t120 t135 t136 
+    => AddIntermediate t135 t136 t139 "0" "1" 
+    => Cons t128 "" t121
+    => AddSingle "0" t128 t139 "1" 
+    => Proxy @Symbol "10"
+-}
+--else instance (AddSingle augendHead addendHead carry sum) => AddHelper augendHead "" addendHead addendTail carry sum 
 else instance (AddSingle augendHead addendHead headCarry headSum
---, AddHelper1 headCarry headSum "0" tailCarry carry intermediateSum 
-, AddIntermediate headCarry headSum tailCarry carry intermediateSum 
+--, AddIntermediate headCarry headSum "0" carry sumHead 
 , Cons augendTailHead augendTailTail augendTail 
 , Cons addendTailHead addendTailTail addendTail 
-, AddHelper augendTailHead augendTailTail addendTailHead addendTailTail tailCarry tailSum
-, Cons intermediateSum tailSum sum 
+, Cons sumTailHead sumTailTail tailSum 
+, AddHelper augendTailHead augendTailTail addendTailHead addendTailTail headCarry "0" sumTailHead sumTailTail
 ) => 
-    AddHelper augendHead augendTail addendHead addendTail carry sum 
+    AddHelper augendHead augendTail addendHead addendTail headCarry "0" headSum  tailSum
 
 
 class Add (augend :: Symbol) (addend :: Symbol) (carry :: Symbol) (sum :: Symbol) | augend addend -> carry sum 
 instance (Cons augendHead augendTail augend, 
     Cons addendHead addendTail addend, 
-    AddHelper augendHead augendTail addendHead addendTail carry sum) => Add augend addend carry sum 
+    Cons sumHead sumTail sum,
+    AddHelper augendHead augendTail addendHead addendTail carryHead carryTail sumHead sumTail) => Add augend addend carryHead sum 
+
+
+term :: forall sym. Proxy sym
+term = Proxy 
+
+sum :: forall augend addend carry sum result. 
+    Add augend addend carry sum => Cons carry sum result => Proxy augend -> Proxy addend -> Proxy result 
+sum _ _ = Proxy 
+
+take :: forall augend addend carry sum result. 
+    Add augend addend carry sum =>
+    Cons carry sum result => 
+    Proxy augend -> Proxy result -> Proxy augend
+take _ _ = Proxy 
+
+
+s :: Proxy "18"
+s = sum (term :: _ "9") (term :: _ "9")
+
+s2 :: Proxy "019"
+s2 = sum (term :: _ "10") (term :: _ "09")
+
+
+t :: Proxy "04363"
+t = sum (term :: _ "4220") (term :: _ "0143")
+
+u :: Proxy "040787"
+u = sum (term :: _ "17220") (term :: _ "23567")
+
+v :: Proxy "077868"
+v = sum (term :: _ "23423") (term :: _ "54445")
+
+--x :: Proxy "03543525"
+x :: Proxy "03543525"
+x = sum (term :: _ "1231293") (term :: _ "2312232")
+
+--z :: Proxy "4"
+z = take (term :: _ "4") (term :: _ "04")
+
+headSym :: forall h t v. Cons h t v => Proxy v -> Proxy h
+headSym _ = Proxy 
+
+tailSym :: forall h t v. Cons h t v => Proxy v -> Proxy t
+tailSym _ = Proxy 
+
+z2 :: Proxy "0"
+z2 = headSym (term :: _ "04")
+
+z3 :: Proxy "4"
+z3 = tailSym (term :: _ "04")
+
+--z1 :: Proxy "10"
+z1 = take (term :: _ "10") (term :: _ "011")
+
+-- z4 :: Proxy "9"
+-- z4 = take (term :: _ "9") (term :: _ "10")
+
+-- z5 :: Proxy "11"
+-- z5 = take (term :: _ "11") (term :: _ "011")
+
+-- z6 :: Proxy "011"
+-- z6 = take (term :: _ "12") (term :: _ "011")
