c16eae7a41524b465033a8f7e2def6d5c47bde85
sigma-andex <sigma.andex@pm.me>
2021-08-11 21:37:57 +0100

    Add singleton

--- src/Data/FastVect/FastVect.purs
+++ src/Data/FastVect/FastVect.purs
@@ -2,6 +2,7 @@ module Data.FastVect.FastVect
   ( Vect
   , replicate
   , empty
+  , singleton
   , append
   , drop
   , take
@@ -9,11 +10,11 @@ module Data.FastVect.FastVect
 
 import Prelude
 import Data.Array as A
-import Data.FastVect.Add (class Add)
+import Data.FastVect.Add (class Add, term)
 import Data.FastVect.ToInt (class ToInt, toInt)
 import Data.Symbol (class IsSymbol, reflectSymbol)
 import Prim.Symbol (class Cons)
-import Type.Proxy (Proxy(..))
+import Type.Proxy (Proxy)
 
 data Vect ∷ Symbol → Type → Type
 data Vect len elem
@@ -29,13 +30,16 @@ replicate ∷ ∀ len elem. ToInt len ⇒ Proxy len → elem → Vect len elem
 replicate proxy elem = Vect proxy $ A.replicate (toInt proxy) elem
 
 empty ∷ ∀ elem. Vect "0" elem
-empty = Vect (Proxy ∷ Proxy "0") []
+empty = Vect (term ∷ _ "0") []
+
+singleton ∷ ∀ elem. elem → Vect "1" elem
+singleton elem = Vect (term ∷ _ "1") [ elem ]
 
 append ∷ ∀ m n elem carry sum m_plus_n. Cons carry sum m_plus_n ⇒ Add m n carry sum ⇒ Vect m elem → Vect n elem → Vect m_plus_n elem
-append (Vect _ xs) (Vect _ ys) = Vect (Proxy ∷ Proxy m_plus_n) (xs <> ys)
+append (Vect _ xs) (Vect _ ys) = Vect (term ∷ _ m_plus_n) (xs <> ys)
 
 drop ∷ ∀ m n elem carry sum m_plus_n. ToInt m ⇒ Cons carry sum m_plus_n ⇒ Add m n carry sum ⇒ Proxy m → Vect m_plus_n elem → Vect n elem
-drop proxy (Vect _ xs) = Vect (Proxy ∷ Proxy n) (A.drop (toInt proxy) xs)
+drop proxy (Vect _ xs) = Vect (term ∷ _ n) (A.drop (toInt proxy) xs)
 
 take ∷ ∀ m n elem carry sum m_plus_n. ToInt m ⇒ Cons carry sum m_plus_n ⇒ Add m n carry sum ⇒ Proxy m → Vect m_plus_n elem → Vect m elem
 take proxy (Vect _ xs) = Vect proxy (A.take (toInt proxy) xs)
