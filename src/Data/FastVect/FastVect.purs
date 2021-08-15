11a0b56cb211747850254e04104bb1f5e87ab0e1
sigma-andex <sigma.andex@pm.me>
2021-08-15 18:14:15 +0100

    Rename AlignToSum

--- src/Data/FastVect/FastVect.purs
+++ src/Data/FastVect/FastVect.purs
@@ -13,7 +13,7 @@ module Data.FastVect.FastVect
 import Prelude
 import Data.Array (unsafeIndex)
 import Data.Array as A
-import Data.FastVect.Add (class Add, class AlignToSum, class Trim, term)
+import Data.FastVect.Add (class Add, class PadZeroes, class Trim, term)
 import Data.FastVect.ToInt (class ToInt, toInt)
 import Data.Symbol (class IsSymbol, reflectSymbol)
 import Partial.Unsafe (unsafePartial)
@@ -48,7 +48,7 @@ singleton elem = Vect (term ∷ _ "1") [ elem ]
 
 append ∷
   ∀ m n elem carry sum m_plus_n_untrimmed m_plus_n m_aligned n_aligned.
-  AlignToSum m n m_aligned n_aligned ⇒
+  PadZeroes m n m_aligned n_aligned ⇒
   Add m_aligned n_aligned carry sum ⇒
   Cons carry sum m_plus_n_untrimmed ⇒
   Trim m_plus_n_untrimmed m_plus_n ⇒
@@ -58,35 +58,36 @@ append (Vect _ xs) (Vect _ ys) = Vect (term ∷ _ m_plus_n) (xs <> ys)
 drop ∷
   ∀ m n elem m_aligned m_plus_n_aligned n_untrimmed m_plus_n.
   ToInt m ⇒
-  AlignToSum m m_plus_n m_aligned m_plus_n_aligned ⇒
+  PadZeroes m m_plus_n m_aligned m_plus_n_aligned ⇒
   Add m_aligned n_untrimmed "0" m_plus_n_aligned ⇒
   Trim n_untrimmed n ⇒
   Proxy m → Vect m_plus_n elem → Vect n elem
 drop proxy (Vect _ xs) = Vect (term ∷ _ n) (A.drop (toInt proxy) xs)
 
 take ∷
-  ∀ m n elem m_plus_n m_aligned m_aligned_plus_n.
+  ∀ m n elem m_plus_n m_aligned m_aligned_plus_n n_untrimmed.
   ToInt m ⇒
-  AlignToSum m m_plus_n m_aligned m_aligned_plus_n ⇒
-  Add m_aligned n "0" m_aligned_plus_n ⇒
+  PadZeroes m m_plus_n m_aligned m_aligned_plus_n ⇒
+  Add m_aligned n_untrimmed "0" m_aligned_plus_n ⇒
+  Trim n_untrimmed n ⇒
   Proxy m → Vect m_plus_n elem → Vect m elem
 take proxy (Vect _ xs) = Vect proxy (A.take (toInt proxy) xs)
 
 index ∷
   ∀ m m_minus_one aligned_one m_aligned m_aligned_minus_one i i_aligned n elem.
   ToInt i ⇒
-  AlignToSum "1" m aligned_one m_aligned ⇒
+  PadZeroes "1" m aligned_one m_aligned ⇒
   Add aligned_one m_minus_one "0" m_aligned ⇒
-  AlignToSum i m_minus_one i_aligned m_aligned_minus_one ⇒
+  PadZeroes i m_minus_one i_aligned m_aligned_minus_one ⇒
   Add i_aligned n "0" m_aligned_minus_one ⇒
   Proxy i → Vect m elem → elem
 index proxy (Vect _ xs) = unsafePartial $ unsafeIndex xs (toInt proxy)
 
 head ∷
   ∀ m m_minus_one aligned_one m_aligned m_aligned_minus_one i_aligned n elem.
-  AlignToSum "1" m aligned_one m_aligned ⇒
+  PadZeroes "1" m aligned_one m_aligned ⇒
   Add aligned_one m_minus_one "0" m_aligned ⇒
-  AlignToSum "0" m_minus_one i_aligned m_aligned_minus_one ⇒
+  PadZeroes "0" m_minus_one i_aligned m_aligned_minus_one ⇒
   Add i_aligned n "0" m_aligned_minus_one ⇒
   Vect m elem → elem
 head = index (term ∷ _ "0")
