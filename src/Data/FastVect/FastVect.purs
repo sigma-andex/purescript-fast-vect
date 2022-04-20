5f3fd8fa71aef7c87bc02cccbef7b082d40351d4
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-04-20 22:14:34 +0100

    Update to use typelevel ints

--- src/Data/FastVect/FastVect.purs
+++ src/Data/FastVect/FastVect.purs
@@ -6,41 +6,51 @@ module Data.FastVect.FastVect
   , append
   , drop
   , take
+  , splitAt
   , index
   , head
-  , splitAt
   , fromArray
   , toArray
   , adjust
   , adjustM
   , cons
+  , term
+  , toInt
   , (:)
-  ) where
+  )
+  where
 
 import Prelude
-import Data.Array (length, unsafeIndex)
+
+import Data.Array (unsafeIndex)
 import Data.Array as A
-import Typelevel.Arithmetic.Add (class Add, class PadZeroes, class Trim, term)
-import Typelevel.Arithmetic.ToInt (class ToInt, toInt)
+import Data.Array as Array
 import Data.Maybe (Maybe(..))
 import Data.Ord (abs)
-import Data.Symbol (class IsSymbol, reflectSymbol)
+import Data.Reflectable (class Reflectable, reflectType)
 import Partial.Unsafe (unsafePartial)
-import Prim.Symbol (class Cons)
-import Type.Proxy (Proxy)
+import Prim.Int (class Add, class Compare)
+import Prim.Ordering (EQ, GT, LT)
+import Type.Proxy (Proxy(..))
 
-data Vect ∷ Symbol → Type → Type
+term :: forall (i :: Int). Proxy i
+term = Proxy
+
+toInt :: forall (len :: Int). Reflectable len Int => Proxy len -> Int
+toInt = reflectType
+
+data Vect ∷ Int → Type → Type
 -- | A Vector: A list-like data structure that encodes it's length in the type, backed by an `Array`.
 -- | 
 -- | ```
--- | vect :: Vect "1" String
+-- | vect :: Vect 1 String
 -- | vect = singleton "a"
 -- | ```
 data Vect len elem
   = Vect (Proxy len) (Array elem)
 
-instance (IsSymbol len, Show elem) ⇒ Show (Vect len elem) where
-  show (Vect proxy elems) = "Vect " <> (reflectSymbol proxy) <> " " <> show elems
+instance (Show elem, Reflectable len Int) ⇒ Show (Vect len elem) where
+  show (Vect proxy elems) = "Vect " <> show (toInt proxy) <> " " <> show elems
 
 instance Eq elem ⇒ Eq (Vect len elem) where
   eq (Vect _ arr1) (Vect _ arr2) = eq arr1 arr2
@@ -48,200 +58,217 @@ instance Eq elem ⇒ Eq (Vect len elem) where
 instance Functor (Vect len) where
   map f (Vect proxy xs) = Vect proxy (map f xs)
 
--- | Create a `Vect` by replicating `len` times the given element 
--- | 
--- | ```
--- | vect :: Vect "300" String
--- | vect = replicate (term :: _ "300") "a"
--- | ```
+
+-- -- | Create a `Vect` by replicating `len` times the given element 
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 300 String
+-- -- | vect = replicate (term :: _ 300) "a"
+-- -- | ```
 replicate ∷
   ∀ len elem.
-  ToInt len ⇒
-  Proxy len → elem → Vect len elem
+  Compare len (-1) GT => 
+  Reflectable len Int => Proxy len → elem → Vect len elem
 replicate proxy elem = Vect proxy $ A.replicate (toInt proxy) elem
 
--- | Creates the empty `Vect`.
--- | 
--- | ```
--- | vect :: Vect "0" String
--- | vect = empty
--- | ```
+
+-- -- | Creates the empty `Vect`.
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 0 String
+-- -- | vect = empty
+-- -- | ```
 empty ∷
   ∀ elem.
-  Vect "0" elem
-empty = Vect (term ∷ _ "0") []
+  Vect 0 elem
+empty = Vect (term ∷ _ 0) []
 
--- | Create a `Vect` of one element.
--- | 
--- | ```
--- | vect :: Vect "1" String
--- | vect = singleton "a"
--- | ```
+-- -- | Create a `Vect` of one element.
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 1 String
+-- -- | vect = singleton "a"
+-- -- | ```
 singleton ∷
   ∀ elem.
-  elem → Vect "1" elem
-singleton elem = Vect (term ∷ _ "1") [ elem ]
+  elem → Vect 1 elem
+singleton elem = Vect (term ∷ _ 1) [ elem ]
 
--- | Append two `Vect`s.
--- | 
--- | ```
--- | as :: Vect "300" String
--- | as = replicate (term :: _ "300") "a"
--- | 
--- | bs :: Vect "200" String
--- | bs = replicate (term :: _ "200") "b"
--- | 
--- | cs :: Vect "500" String
--- | cs = append as bs
--- | ```
+-- -- | Append two `Vect`s.
+-- -- | 
+-- -- | ```
+-- -- | as :: Vect 300 String
+-- -- | as = replicate (term :: _ 300) "a"
+-- -- | 
+-- -- | bs :: Vect 200 String
+-- -- | bs = replicate (term :: _ 200) "b"
+-- -- | 
+-- -- | cs :: Vect 500 String
+-- -- | cs = append as bs
+-- -- | ```
 append ∷
-  ∀ m n elem carry sum m_plus_n_untrimmed m_plus_n m_aligned n_aligned.
-  PadZeroes m n m_aligned n_aligned ⇒
-  Add m_aligned n_aligned carry sum ⇒
-  Cons carry sum m_plus_n_untrimmed ⇒
-  Trim m_plus_n_untrimmed m_plus_n ⇒
+  ∀ m n m_plus_n elem.
+  Add m n m_plus_n ⇒
+  Compare m (-1) GT => 
+  Compare n (-1) GT => 
   Vect m elem → Vect n elem → Vect m_plus_n elem
-append (Vect _ xs) (Vect _ ys) = Vect (term ∷ _ m_plus_n) (xs <> ys)
+append (Vect _ xs) (Vect _ ys) = Vect (Proxy ∷ _ m_plus_n) (xs <> ys)
 
--- | Safely drop `m` elements from a `Vect`. 
--- | Will result in a compile-time error if you are trying to drop more elements than exist in the vector.
--- | 
--- | ```
--- | vect :: Vect "300" String
--- | vect = replicate (term :: _ "300") "a"
--- | 
--- | newVect :: Vect "200" String
--- | newVect = drop (term :: _ "100") vect
--- | ```
+-- -- | Safely drop `m` elements from a `Vect`. 
+-- -- | Will result in a compile-time error if you are trying to drop more elements than exist in the vector.
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 300 String
+-- -- | vect = replicate (term :: _ 300) "a"
+-- -- | 
+-- -- | newVect :: Vect 200 String
+-- -- | newVect = drop (term :: _ 100) vect
+-- -- | ```
 drop ∷
-  ∀ m n elem m_aligned m_plus_n_aligned n_untrimmed m_plus_n.
-  ToInt m ⇒
-  PadZeroes m m_plus_n m_aligned m_plus_n_aligned ⇒
-  Add m_aligned n_untrimmed "0" m_plus_n_aligned ⇒
-  Trim n_untrimmed n ⇒
+  ∀ m n m_plus_n elem.
+  Add m n m_plus_n ⇒
+  Reflectable m Int => 
+  Compare m (-1) GT => 
   Proxy m → Vect m_plus_n elem → Vect n elem
 drop proxy (Vect _ xs) = Vect (term ∷ _ n) (A.drop (toInt proxy) xs)
 
--- | Safely take `m` elements from a `Vect`.
--- | Will result in a compile-time error if you are trying to take more elements than exist in the vector.
--- | 
--- | ```
--- | vect :: Vect "300" String
--- | vect = replicate (term :: _ "300") "a"
--- | 
--- | newVect :: Vect "100" String
--- | newVect = take (term :: _ "100") vect
--- | ```
+-- -- | Safely take `m` elements from a `Vect`.
+-- -- | Will result in a compile-time error if you are trying to take more elements than exist in the vector.
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 300 String
+-- -- | vect = replicate (term :: _ 300) "a"
+-- -- | 
+-- -- | newVect :: Vect 100 String
+-- -- | newVect = take (term :: _ 100) vect
+-- -- | ```
 take ∷
-  ∀ m n elem m_plus_n m_aligned m_aligned_plus_n n_untrimmed.
-  ToInt m ⇒
-  PadZeroes m m_plus_n m_aligned m_aligned_plus_n ⇒
-  Add m_aligned n_untrimmed "0" m_aligned_plus_n ⇒
-  Trim n_untrimmed n ⇒
+  ∀ m n m_plus_n elem.
+  Add m n m_plus_n ⇒
+  Reflectable m Int => 
+  Compare m (-1) GT => 
   Proxy m → Vect m_plus_n elem → Vect m elem
 take proxy (Vect _ xs) = Vect proxy (A.take (toInt proxy) xs)
 
--- | Split the `Vect` into two sub vectors `before` and `after`, where before contains up to `m` elements.
--- | 
--- | ```
--- | vect ∷ Vect "10" String
--- | vect = replicate (term ∷ _ "10") "a"
--- | 
--- | split ∷
--- |   { after ∷ Vect "7" String
--- |   , before ∷ Vect "3" String
--- |   }
--- | split = splitAt (term ∷ _ "3") vect
--- | ```
+-- -- | Split the `Vect` into two sub vectors `before` and `after`, where before contains up to `m` elements.
+-- -- | 
+-- -- | ```
+-- -- | vect ∷ Vect 10 String
+-- -- | vect = replicate (term ∷ _ 10) "a"
+-- -- | 
+-- -- | split ∷
+-- -- |   { after ∷ Vect 7 String
+-- -- |   , before ∷ Vect 3 String
+-- -- |   }
+-- -- | split = splitAt (term ∷ _ 3) vect
+-- -- | ```
 splitAt ∷
-  ∀ m n elem m_aligned m_plus_n_aligned n_untrimmed m_plus_n.
-  ToInt m ⇒
-  PadZeroes m m_plus_n m_aligned m_plus_n_aligned ⇒
-  Add m_aligned n_untrimmed "0" m_plus_n_aligned ⇒
-  Trim n_untrimmed n ⇒
+  ∀ m n m_plus_n elem.
+  Add m n m_plus_n ⇒
+  Reflectable m Int => 
+  Compare m (-1) GT => 
+  Compare n (-1) GT => 
   Proxy m → Vect m_plus_n elem → { before ∷ Vect m elem, after ∷ Vect n elem }
 splitAt proxy (Vect _ xs) = { before: Vect proxy before, after: Vect (term ∷ _ n) after }
   where
   { before, after } = A.splitAt (toInt proxy) xs
 
--- | Safely access the `i`-th element of a `Vect`.
--- | 
--- | ```
--- | vect :: Vect "300" String
--- | vect = replicate (term :: _ "300") "a"
--- | 
--- | elem :: String
--- | elem = index (term :: _ "299") vect
--- | ```
+-- -- | Safely access the `i`-th element of a `Vect`.
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 300 String
+-- -- | vect = replicate (term :: _ 300) "a"
+-- -- | 
+-- -- | elem :: String
+-- -- | elem = index (term :: _ 299) vect
+-- -- | ```
 index ∷
-  ∀ m m_minus_one aligned_one m_aligned m_aligned_minus_one i i_aligned n elem.
-  ToInt i ⇒
-  PadZeroes "1" m aligned_one m_aligned ⇒
-  Add aligned_one m_minus_one "0" m_aligned ⇒
-  PadZeroes i m_minus_one i_aligned m_aligned_minus_one ⇒
-  Add i_aligned n "0" m_aligned_minus_one ⇒
+  ∀ m m_minus_one i n elem.
+  Compare m_minus_one (-1) GT => 
+  Add 1 m_minus_one m ⇒
+  Compare n (-1) GT =>
+  Add i n m_minus_one ⇒
+  Compare i (-1) GT => 
+  Reflectable i Int => 
   Proxy i → Vect m elem → elem
 index proxy (Vect _ xs) = unsafePartial $ unsafeIndex xs (toInt proxy)
 
--- | Safely access the head of a `Vect`.
--- | 
--- | ```
--- | vect :: Vect "300" String
--- | vect = replicate (term :: _ "300") "a"
--- | 
--- | elem :: String
--- | elem = head vect
--- | ```
+-- -- | Safely access the head of a `Vect`.
+-- -- | 
+-- -- | ```
+-- -- | vect :: Vect 300 String
+-- -- | vect = replicate (term :: _ 300) "a"
+-- -- | 
+-- -- | elem :: String
+-- -- | elem = head vect
+-- -- | ```
 head ∷
-  ∀ m m_minus_one aligned_one m_aligned m_aligned_minus_one i_aligned n elem.
-  PadZeroes "1" m aligned_one m_aligned ⇒
-  Add aligned_one m_minus_one "0" m_aligned ⇒
-  PadZeroes "0" m_minus_one i_aligned m_aligned_minus_one ⇒
-  Add i_aligned n "0" m_aligned_minus_one ⇒
+  ∀ m m_minus_one i elem.
+  Compare m_minus_one (-1) GT => 
+  Add 1 m_minus_one m ⇒
+  Compare i (-1) GT =>
+  Add 0 i m_minus_one =>
   Vect m elem → elem
-head = index (term ∷ _ "0")
+head = index (term ∷ _ 0)
 
--- | Attempt to create a `Vect` of a given size from an `Array`. 
--- | 
--- | ```
--- | fromArray (term :: _ "3") ["a", "b", "c"] = Just (Vect (term :: _ "3") ["a", "b", "c"])
--- | 
--- | fromArray (term :: _ "4") ["a", "b", "c"] = Nothing
--- | ```
-fromArray ∷ ∀ len elem. ToInt len ⇒ Proxy len → Array elem → Maybe (Vect len elem)
-fromArray proxy array | length array == toInt proxy = Just (Vect proxy array)
+-- -- | Attempt to create a `Vect` of a given size from an `Array`. 
+-- -- | 
+-- -- | ```
+-- -- | fromArray (term :: _ 3) ["a", "b", "c"] = Just (Vect (term :: _ 3) ["a", "b", "c"])
+-- -- | 
+-- -- | fromArray (term :: _ 4) ["a", "b", "c"] = Nothing
+-- -- | ```
+fromArray ∷ ∀ len elem. 
+  Reflectable len Int => 
+  Compare len (-1) GT => 
+  Proxy len → 
+  Array elem → 
+  Maybe (Vect len elem)
+fromArray proxy array | Array.length array == toInt proxy = Just (Vect proxy array)
 fromArray _ _ = Nothing
 
--- | Converts the `Vect` to an `Array`, effectively dropping the size information.
-toArray ∷ ∀ len elem. Vect len elem → Array elem
+-- -- | Converts the `Vect` to an `Array`, effectively dropping the size information.
+toArray ∷ ∀ len elem. 
+  Compare len (-1) GT => 
+  Vect len elem → Array elem
 toArray (Vect _ arr) = arr
 
--- | Creates a `Vect` by adjusting the given `Array`, padding with the provided element if the array is to small or dropping elements if the array is to big.
--- | 
--- | ```
--- | toArray $ adjust (term ∷ _ "10") 0 [ 1, 2, 3 ] == [ 0, 0, 0, 0, 0, 0, 0, 1, 2, 3 ]
--- | 
--- | toArray $ adjust (term ∷ _ "3") 0 [ 0, 0, 0, 0, 1, 2, 3 ] == [ 1, 2, 3 ]
--- | ```
-adjust ∷ ∀ len elem. ToInt len ⇒ Proxy len → elem → Array elem → Vect len elem
-adjust proxy elem array = case length array - toInt proxy of
+-- -- | Creates a `Vect` by adjusting the given `Array`, padding with the provided element if the array is to small or dropping elements if the array is to big.
+-- -- | 
+-- -- | ```
+-- -- | toArray $ adjust (term ∷ _ 10) 0 [ 1, 2, 3 ] == [ 0, 0, 0, 0, 0, 0, 0, 1, 2, 3 ]
+-- -- | 
+-- -- | toArray $ adjust (term ∷ _ 3) 0 [ 0, 0, 0, 0, 1, 2, 3 ] == [ 1, 2, 3 ]
+-- -- | ```
+adjust ∷ ∀ len elem. 
+  Reflectable len Int => 
+  Compare len (-1) GT => 
+  Proxy len → 
+  elem → 
+  Array elem → 
+  Vect len elem
+adjust proxy elem array = case Array.length array - toInt proxy of
   0 → Vect proxy array
   len | len < 0 → Vect proxy $ A.replicate (abs len) elem <> array
   len → Vect proxy $ A.drop len array
 
--- | Like `adjust` but uses the Moinoid instance of elem to create the elements.
-adjustM ∷ ∀ len elem. Monoid elem ⇒ ToInt len ⇒ Proxy len → Array elem → Vect len elem
+-- -- | Like `adjust` but uses the Moinoid instance of elem to create the elements.
+adjustM ∷ ∀ len elem. 
+  Monoid elem ⇒ 
+  Reflectable len Int => 
+  Compare len (-1) GT => 
+  Proxy len → 
+  Array elem → 
+  Vect len elem
 adjustM proxy = adjust proxy mempty
 
--- | Attaches an element to the front of the `Vect`, creating a new `Vect` with size incremented. 
--- | 
--- | Note, the running time of this function is `O(n)`.
+-- -- | Attaches an element to the front of the `Vect`, creating a new `Vect` with size incremented. 
+-- -- | 
+-- -- | Note, the running time of this function is `O(n)`.
 cons ∷
-  ∀ len elem one_aligned len_aligned carry sum len_plus_1_untrimmed len_plus_1.
-  PadZeroes "1" len one_aligned len_aligned ⇒
-  Add one_aligned len carry sum ⇒
-  Cons carry sum len_plus_1_untrimmed ⇒
-  Trim len_plus_1_untrimmed len_plus_1 ⇒
+  ∀ len len_plus_1 elem.
+  Add 1 len len_plus_1 ⇒
+  Compare len (-1) GT => 
   elem → Vect len elem → Vect len_plus_1 elem
 cons elem (Vect _ arr) = Vect (term ∷ _ len_plus_1) (A.cons elem arr)
 
--- test/Data/FastVect/FastVectSpec.purs
+++ test/Data/FastVect/FastVectSpec.purs
@@ -1,11 +1,11 @@
 module Data.FastVect.FastVectSpec where
 
 import Data.FastVect.FastVect
-import Typelevel.Arithmetic.Add (term)
+
 import Data.Maybe (Maybe(..))
-import Prelude (Unit, discard)
+import Prelude (Unit, discard, pure, ($))
 import Test.Spec (Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (expectError, shouldEqual)
 
 spec ∷ Spec Unit
 spec =
@@ -13,34 +13,53 @@ spec =
     describe "fromArray" do
       it "should create a Vect from an Array" do
         let
-          actualSuccess ∷ Maybe (Vect "3" String)
-          actualSuccess = fromArray (term ∷ _ "3") [ "a", "b", "c" ]
+          actualSuccess ∷ Maybe (Vect 3 String)
+          actualSuccess = fromArray (term ∷ _ 3) [ "a", "b", "c" ]
 
           expectedSuccess = append (singleton "a") (append (singleton "b") (singleton "c"))
-          actualFail1 ∷ Maybe (Vect "4" String)
-          actualFail1 = fromArray (term ∷ _ "4") [ "a", "b", "c" ]
+          actualFail1 ∷ Maybe (Vect 4 String)
+          actualFail1 = fromArray (term ∷ _ 4) [ "a", "b", "c" ]
 
-          actualFail2 ∷ Maybe (Vect "2" String)
-          actualFail2 = fromArray (term ∷ _ "2") [ "a", "b", "c" ]
+          actualFail2 ∷ Maybe (Vect 2 String)
+          actualFail2 = fromArray (term ∷ _ 2) [ "a", "b", "c" ]
         actualSuccess `shouldEqual` (Just expectedSuccess)
         actualFail1 `shouldEqual` Nothing
         actualFail2 `shouldEqual` Nothing
+
+      it "should successfully acccess elements from a Vect" do
+        let 
+          vect = 1 : 2 : 3 : 4 : empty
+        (head vect) `shouldEqual` 1
+        --(head empty) `shouldEqual` 1 -- should not compile
+        (index (term :: _ 0) vect) `shouldEqual` 1
+        (index (term :: _ 3) vect) `shouldEqual` 4
+        --(index (term :: _ 4) vect) `shouldEqual` 1 -- should not compile
+
+        (drop (term :: _ 4) vect) `shouldEqual` empty
+        (drop (term :: _ 3) vect) `shouldEqual` (singleton 4)
+        --(drop (term :: _ 5) vect) `shouldEqual` (singleton 4) -- should not compile
+
+        (take (term :: _ 4) vect) `shouldEqual` vect
+        (take (term :: _ 3) vect) `shouldEqual` (1 : 2 : 3 : empty)
+        --(take (term :: _ 5) vect) `shouldEqual` (1 : 2 : 3 : empty)
+
       it "should adjust an Array to a Vect" do
         let
           expectedPad = [ 0, 0, 0, 0, 0, 0, 0, 1, 2, 3 ]
 
-          actualPad = adjust (term ∷ _ "10") 0 [ 1, 2, 3 ]
+          actualPad = adjust (term ∷ _ 10) 0 [ 1, 2, 3 ]
 
           expectedDrop = [ 1, 2, 3 ]
 
-          actualDrop = adjust (term ∷ _ "3") 0 [ 0, 0, 0, 0, 1, 2, 3 ]
+          actualDrop = adjust (term ∷ _ 3) 0 [ 0, 0, 0, 0, 1, 2, 3 ]
 
           expectedEqual = [ 1, 2, 3, 4, 5 ]
-          actualEqual = adjust (term ∷ _ "5") 0 [ 1, 2, 3, 4, 5 ]
+          actualEqual = adjust (term ∷ _ 5) 0 [ 1, 2, 3, 4, 5 ]
 
           expectedPadM = [ "", "", "", "", "a", "b", "c" ]
-          actualPadM = adjustM (term ∷ _ "7") [ "a", "b", "c" ]
+          actualPadM = adjustM (term ∷ _ 7) [ "a", "b", "c" ]
         (toArray actualPad) `shouldEqual` expectedPad
         (toArray actualDrop) `shouldEqual` expectedDrop
         (toArray actualEqual) `shouldEqual` expectedEqual
         (toArray actualPadM) `shouldEqual` expectedPadM
+
